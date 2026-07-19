1a560e97092e17d7f2429bd270ec7d249b8a36be
Kenneth Albelda <251307570+kbalbelda-mlai@users.noreply.github.com>
2026-07-19 21:12:07 +0800

    Fixed local patching

---
+++ astro.owner.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+
+export default defineConfig({
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  site: "http://localhost:4321",
+  base: "/",
+});
