615c5f1e4748434d8a7b7ce432a97d5648e40cdf
Kenneth Albelda <251307570+kbalbelda-mlai@users.noreply.github.com>
2026-07-19 20:38:59 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
-export default defineConfig(({ command }) => {
-  if (command === "dev") {
-    return {
-      output: "server",
-      adapter: node({
-        mode: "standalone",
-      }),
-      site: "http://localhost:4321",
-      base: "/",
-    };
-  }
-
-  return {
-    output: "static",
-    site: "https://kbalbelda-mlai.github.io",
-    base: "/MySkillForest",
-  };
+export default defineConfig({
+  output: "static",
+  site: "https://kbalbelda-mlai.github.io",
+  base: "/MySkillForest",
+  trailingSlash: "always",
 });
